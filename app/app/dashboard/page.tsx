3f5e545274751d6ec41c63b9c1aadac106a903c6
warunie97-sketch <warunie97@gmail.com>
2025-11-01 16:27:08 +0800

    Update page.tsx

--- app/app/dashboard/page.tsx
+++ app/app/dashboard/page.tsx
@@ -1,23 +1,28 @@
+"use client";
+import AuthGuard from "../AuthGuard";
+
 export default function Dashboard() {
   return (
-    <div style={{ padding: "40px" }}>
-      <h2>Dashboard ✨</h2>
-      <p>Selamat datang penulis hebat! 📝</p>
+    <AuthGuard>
+      <div style={{ padding: "40px" }}>
+        <h2>Dashboard ✨</h2>
+        <p>Selamat datang penulis hebat! 📝</p>
 
-      <a
-        href="/auth/login"
-        style={{
-          marginTop: "20px",
-          display: "inline-block",
-          padding: "10px 16px",
-          background: "#E74C3C",
-          color: "white",
-          borderRadius: "6px",
-          textDecoration: "none",
-        }}
-      >
-        Logout
-      </a>
-    </div>
+        <a
+          href="/auth/login"
+          style={{
+            marginTop: "20px",
+            display: "inline-block",
+            padding: "10px 16px",
+            background: "#E74C3C",
+            color: "white",
+            borderRadius: "6px",
+            textDecoration: "none",
+          }}
+        >
+          Logout
+        </a>
+      </div>
+    </AuthGuard>
   );
 }
