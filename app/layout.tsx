3b2f3986f7bb5b68b4b0fcce902e9f1af2a228fe
warunie97-sketch <warunie97@gmail.com>
2025-11-01 15:58:21 +0800

    Delete app directory

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="ms">
-      <body className="min-h-screen bg-slate-50">
-        {children}
-      </body>
-    </html>
-  );
-}
