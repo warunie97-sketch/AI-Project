fa9d01f640195870ab55eda47b1b1ef199fd354c
warunie97-sketch <warunie97@gmail.com>
2025-11-01 16:08:50 +0800

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "AI Writer",
+  description: "AI Assistant untuk penulis ✍️✨",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
